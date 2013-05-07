d0393287dabb5ba4542d3e9087cc452183da007b
Noel Bordy <b1018894@my.shu.ac.uk>
2013-05-07 22:49:13 +0100

    edited registerentry and registrationsheet  domain classes, and manage to make them work

--- grails-app/domain/uk/ac/shu/webarch/eregister/RegistrationSheet.groovy
+++ grails-app/domain/uk/ac/shu/webarch/eregister/RegistrationSheet.groovy
@@ -2,31 +2,24 @@ package uk.ac.shu.webarch.eregister
 
 class RegistrationSheet {
 
-  /* What course this sheet is for */
+
   RegClass regClass
 
-  /* Shortcode */
   String shortcode
 
-  /* Date of the sheet */
+
   Date sheetDate
 
-  /* Signatories */
   Set signatories
 
-  /* Any notes */
+
+
   String notes
 
   static hasMany = [signatories:RegisterEntry]
   static mappedBy = [signatories:'regSheet']
 
-  /** TODO:
-   *  Add the metadata needed to add a list if RegisterEntry classes for this sheet... essentially
-   *  modelling the fact that 1 registration sheet has many register entries.
-   *
-   *  The RegisterEntry class will need a property that points back to this RegistrationSheet. You
-   *  should implement that link first
-   */
+
 
   static constraints = {
     regClass(nullable:false, blank:false);
--- grails-app/domain/uk/ac/shu/webarch/eregister/Student.groovy
+++ grails-app/domain/uk/ac/shu/webarch/eregister/Student.groovy
@@ -6,15 +6,13 @@ class Student {
   String studentNumber
   String notes
 
-  /* The courses this student is enrolled on */
+  
   Set courses
 
-  /* The set of classes this student has attended */
+
   Set classAtts
 
-  /* This demonstrates why the hasMany structure is a list.. in this case we have a
-     domain class with many set objects, so the hasMany metadata needs to specify several
-     different sets */
+ 
   static hasMany = [
     courses: Enrollment,
     classAtts: RegisterEntry
@@ -25,12 +23,13 @@ class Student {
     classAtts:'student'
   ]
 
-  /**
-   * TODO:
-   * Make the notes field use the "text" type, and name the column "student_notes" in the database
-   */
+ 
   static constraints = {
     fullStudentName(nullable:false, blank:false,maxSize:256);
     studentNumber(nullable:false, blank:false,maxSize:256);
   }
+
+static mapping = {
+   notes column: 'student_notes', type:'text'
+}
 }
