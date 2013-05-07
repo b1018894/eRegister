d116627dab2915c391c71e29ed2269e118a8b970
Noel Bordy <b1018894@my.shu.ac.uk>
2013-05-07 20:46:46 +0100

    edited domain classes, and manage to make them work

--- grails-app/domain/uk/ac/shu/webarch/eregister/RegClass.groovy
+++ grails-app/domain/uk/ac/shu/webarch/eregister/RegClass.groovy
@@ -1,7 +1,44 @@
 package uk.ac.shu.webarch.eregister
 
+/*
+ * A Class. 
+ * We called the class RegClass to avoid reserved word clashes with the java keyword "class"
+ *
+ * This class has a name, and a foreign key to the instructor who teaches this class. This is the 1 side
+ * of a 1 to many relationship
+ *
+ */
 class RegClass {
 
-    static constraints = {
-    }
+  /* Class name */
+  String name
+
+  /* Class code - Used in URLs to generate short clean COOL URLs */
+  String code
+
+  /* Instructor teaching this class */
+  Instructor classInstructor
+
+  /* This class is an occurrence of which course? */
+  Course course
+
+  /* The set of students enrolled on this class */
+  Set enrolledSudents
+  Set registrationSheets
+
+  static hasMany = [
+    enrolledStudents:Enrollment, registrationSheets:RegistrationSheet
+  ]
+
+  static mappedBy = [
+    enrolledStudents:'regClass', registrationSheets:'regClass'
+  ]
+
+  static constraints = {
+  }
+
+  static mapping = {
+    classInstructor column:'instructor_fk'
+    course column:'course_fk'
+  }
 }
